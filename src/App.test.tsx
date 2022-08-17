21ed625dec0c8ac444570142e011ee06aa05e6ba
Cesar <ccarabetta@cdt.com.ar>
2022-08-17 09:50:00 -0300

    Iniciando repo testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('button has correct color', () => {
+  render(<App/>)
+  //Encuentra un boton con el nombre "Change to blue"
+  const colorButton = screen.getByRole('button', { name: 'Change to blue'})
+
+  //Espera que el back color sea red
+  expect(colorButton).toHaveStyle({backgroundColor: 'red'})
+
+  //Click
+  fireEvent.click(colorButton);
+  // Expect BG to be blue
+  expect(colorButton).toHaveStyle({backgroundColor: 'blue'})
+  // Expect text to change to "change to red"
+  expect(colorButton.textContent).toBe('Change to red')
+
 });
+test('button has initial text', () => {
+  
+});
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
+import React , { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+  const [ color, setColor ] = useState('red')
+
+  const handleBgChange = () => {
+    color === 'red' ? setColor('blue') : setColor('red')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <button 
+      style={{backgroundColor: `${color}`}}
+      onClick={handleBgChange}>{`Change to ${color === 'red' ? 'blue' : 'red'}`}</button>
     </div>
   );
 }
