959fcecbad91ec535cb44f9856e5e244c8d2eeb1
Cesar <ccarabetta@cdt.com.ar>
2022-09-09 10:08:43 -0300

    siguiendo

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,24 +1,78 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import { replaceCamelWithSpaces } from './App';
 
 test('button has correct color', () => {
-  render(<App/>)
+  render(<App />);
   //Encuentra un boton con el nombre "Change to blue"
-  const colorButton = screen.getByRole('button', { name: 'Change to blue'})
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
 
   //Espera que el back color sea red
-  expect(colorButton).toHaveStyle({backgroundColor: 'red'})
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
 
   //Click
   fireEvent.click(colorButton);
   // Expect BG to be blue
-  expect(colorButton).toHaveStyle({backgroundColor: 'blue'})
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
   // Expect text to change to "change to red"
-  expect(colorButton.textContent).toBe('Change to red')
-
+  expect(colorButton).toHaveTextContent('Change to red');
 });
-test('button has initial text', () => {
-  
+test('initial conditions', () => {
+  render(<App />);
+  // Check that the button starts enabled
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  expect(colorButton).toBeEnabled();
+
+  // Check that the checkbox starts unchecked
+  const checkBox = screen.getByRole('checkbox', { name: 'Disable button' });
+  expect(checkBox).not.toBeChecked();
+
+  // Disparamos el checkbox
+  fireEvent.click(checkBox);
+
+  // Entonces el boton deberia quedar disabled
+  expect(colorButton).toBeDisabled();
 });
 
+test('Boton deshabilitado tiene background gris y vuelve a rojo', () => {
+  render(<App />);
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  const checkBox = screen.getByRole('checkbox', { name: 'Disable button' });
+
+  //Disable button
+  fireEvent.click(checkBox);
+  expect(colorButton).toHaveStyle({ backgroundColor: 'gray' });
+
+  // //Re-enable button
+  fireEvent.click(checkBox);
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+});
+test('Boton cambia de color y luego se deshabilita', () => {
+  render(<App />);
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  const checkBox = screen.getByRole('checkbox', { name: 'Disable button' });
+
+  // Change to blue
+  fireEvent.click(colorButton);
+
+  //Disable button
+  fireEvent.click(checkBox);
+  expect(colorButton).toHaveStyle({ backgroundColor: 'gray' });
+
+  // //Re-enable button
+  fireEvent.click(checkBox);
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+});
+
+describe('sapces before camel-case capital letters', () => {
+  test('Works for no inner capital letters', () => {
+    expect(replaceCamelWithSpaces('Red')).toBe('Red');
+  });
+  test('Works for midnight blue', () => {
+    expect(replaceCamelWithSpaces('MidnightBlue')).toBe('Midnight Blue');
+  });
+  test('Works for multiple inner capital letters', () => {
+    expect(replaceCamelWithSpaces('MediumVioletRed')).toBe('Medium Violet Red');
+  });
+});
--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,45 @@ import React , { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+export function replaceCamelWithSpaces(colorName:string) {
+  return colorName.replace(/\B([A-Z])\B/g, ' $1')
+}
+
 
 function App() {
 
   const [ color, setColor ] = useState('red')
+  const [ disabled, setDisabled ] = useState(false)
 
   const handleBgChange = () => {
     color === 'red' ? setColor('blue') : setColor('red')
   }
+  const handleDisable = (e:any) => {
+    // setDisabled(disabled === false ? true : false)
+    setDisabled(prev => prev = e.target.checked)       
+  }
+  console.log('disabled desde app', disabled)
+
 
   return (
+    <>
     <div className="App"> 
       <button 
-      style={{backgroundColor: `${color}`}}
-      onClick={handleBgChange}>{`Change to ${color === 'red' ? 'blue' : 'red'}`}</button>
+      id="button"
+      style={{backgroundColor: disabled ? 'gray' : color}}
+      onClick={handleBgChange}
+      disabled={disabled}
+      >{`Change to ${color === 'red' ? 'blue' : 'red'}`}      
+      </button> 
+     
+      <hr />
+      <input 
+        type="checkbox" 
+        onChange={ handleDisable }
+        id="disable-button-checkbox"/> 
+        <label htmlFor="disable-button-checkbox">Disable button</label>
     </div>
+    </>
   );
 }
 
